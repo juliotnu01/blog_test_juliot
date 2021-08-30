d83c435e861e90ef338573728ee5261bd1d6ae33
Juliot <developer4@otp.com.co>
2021-08-30 04:42:17 -0500

    commit temporal

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Photo extends Model
 {
     protected $guarded = [];
+
+    function hasManyTags()
+    {
+        return $this->hasMany(TagPhotos::class, 'photo_id');
+    }
 }
